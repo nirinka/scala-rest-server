dc34e52b1e829212298841699a999ad7688e566e
Iryna Romanenko <Iryna.Romanenko@flextrade.com>
2016-03-18 10:29:55 +0000

    Test added

--- src/test/scala/org/library/rest/LibraryServerTest.scala
+++ src/test/scala/org/library/rest/LibraryServerTest.scala
@@ -1,15 +1,21 @@
 package org.library.rest
 
+import org.library.Document
+import org.library.db.DocumentStore
 import org.library.rest.LibraryResponse.AllDocumentsResponse
+import org.scalatest.mock.MockitoSugar
 import org.scalatest.{GivenWhenThen, FeatureSpec}
 import spray.httpx.SprayJsonSupport
 import spray.testkit.ScalatestRouteTest
+import org.mockito.Mockito
 
 
-class LibraryServerTest extends FeatureSpec with GivenWhenThen with ScalatestRouteTest with SprayJsonSupport{
+class LibraryServerTest extends FeatureSpec with GivenWhenThen with ScalatestRouteTest with SprayJsonSupport with MockitoSugar{
   import org.library.rest.Protocols._
 
   val documentEndpoint = "/document"
+  val documentStore = mock[DocumentStore]
+  val document = Document("Good Comment", "Comment", Set("good", "comment"), "This is a good comment indeed")
 
   var libraryServer: LibraryServer = {
     val server: LibraryServer = new LibraryServer(8000, "localhost")
@@ -19,11 +25,12 @@ class LibraryServerTest extends FeatureSpec with GivenWhenThen with ScalatestRou
 
   feature("Get Documents"){
     scenario("Get all documents") {
-      Given("")
+      Given("Library has one document")
+      Mockito.when(documentStore.getAll).thenReturn(Seq(document))
       When("Get all documents request receved")
       Get(documentEndpoint) ~> libraryServer.routes() ~> check {
-        Then("Response with the list of all document is returned")
-        assertResult(AllDocumentsResponse)
+        Then("Response with the document from library is returned")
+        assertResult(AllDocumentsResponse(Seq(document)), "Should get expected documents")(responseAs[AllDocumentsResponse])
       }
     }
   }
